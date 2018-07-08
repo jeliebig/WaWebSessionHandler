185823b4586da59c65e41ea8d7ccc0f33e3c589f
Julian Liebig <je.liebig@web.de>
2018-07-08 14:11:37 +0200

    fixed missing files

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ import py2exe
 
 # you may have to change paths and copy those files to dist dir
 wd_path = 'D:\\Python27\\Lib\\site-packages\\selenium\\webdriver'
-required_data_files = ['geckodriver.exe', 'chromedriver.exe', ('selenium/webdriver/firefox',
-                        ['{}\\firefox\\webdriver.xpi'.format(wd_path), '{}\\remote\\isDisplayed.js'.format(wd_path),
-                         '{}\\firefox\\webdriver_prefs.json'.format(wd_path), '{}\\remote\\getAttribute.js'.format(wd_path)])]
+required_data_files = ['geckodriver.exe', 'chromedriver.exe',
+                       ('selenium/webdriver/remote',
+                        ['{}\\remote\\isDisplayed.js'.format(wd_path),
+                         '{}\\remote\\getAttribute.js'.format(wd_path)]),
+                       ('selenium/webdriver/firefox',
+                        ['{}\\firefox\\webdriver.xpi'.format(wd_path),
+                         '{}\\firefox\\webdriver_prefs.json'.format(wd_path)])]
 
 setup(
-    console = ['WAWebTransfer.py'],
-    data_files = required_data_files,
-    options = {
-               "py2exe":{
-                   "skip_archive": True,
-                        }
-               }
+    console=['WADisplay.py'],
+    data_files=required_data_files,
+    options={
+        "py2exe": {
+            "skip_archive": True,
+        }
+    }
 )
