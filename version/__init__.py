e305b77e9514f96f0ba0468584ca2034b05f35f4
Julian Liebig <je.liebig@web.de>
2021-11-05 14:19:13 +0100

    Fix docstring for get_version()

--- version/__init__.py
+++ version/__init__.py
@@ -26,7 +26,9 @@ versions = [
 def get_version(wa_session: SessionObject) -> Optional[Version]:
     """
     Create a :class:`Version` object from a :class:`SessionObject` if possible
-    :param wa_session: the session of which a :class:`Version` object should be created from
+
+    :param wa_session: the session of which a `Version` object should be created from
+
     :return: the resulting `Version` object or `None` if no matching version was found
     """
     for version in versions:
